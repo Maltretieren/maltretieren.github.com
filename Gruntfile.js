08e2d195529033162cc1651265f38918f23e81c7
crunsh <maltretieren.github.com>
2014-10-17 22:18:52 +0200

    test

--- Gruntfile.js
+++ Gruntfile.js
@@ -7,11 +7,6 @@ module.exports = function(grunt) {
                 configFile: 'tests/config/karma.conf.js',
                 singleRun: true,
                 browsers: ['PhantomJS']
-            },
-            coveralls: {
-                options: {
-                    coverage_dir: 'tests/coverage/'
-                }
             }
         }
     });
