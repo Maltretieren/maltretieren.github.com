a9c46fd4d34aa2c776038a0d84c78c88d5ebfeb5
Malteser <MalteAhrens@arcor.de>
2014-10-20 14:30:25 +0200

    test

--- Gruntfile.js
+++ Gruntfile.js
@@ -11,6 +11,7 @@ module.exports = function(grunt) {
 			travis: {
 			    configFile: 'tests/config/karma.conf.js',
                 singleRun: true,
+				background: false,
                 browsers: ['PhantomJS']
 			}
         },
