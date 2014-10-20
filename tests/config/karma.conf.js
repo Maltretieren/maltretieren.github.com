a9029452c4f0bb2b49ad75c626f64799b2c1bcfe
Malteser <MalteAhrens@arcor.de>
2014-10-20 10:19:37 +0200

    test

--- tests/config/karma.conf.js
+++ tests/config/karma.conf.js
@@ -68,7 +68,8 @@ module.exports = function(config){
 		// optionally, configure the reporter
 		coverageReporter: {
 		  type : 'text',
-		  dir : 'tests/coverage/'
+		  dir : 'tests/coverage/report',
+		  subdir: '.'
 		}
 		
     })
