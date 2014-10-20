5990a4f94206a68455b475b2f817e1d79a90ab42
Malteser <MalteAhrens@arcor.de>
2014-10-20 13:31:44 +0200

    test

--- tests/config/karma.conf.js
+++ tests/config/karma.conf.js
@@ -55,7 +55,7 @@ module.exports = function(config){
 		// http://karma-runner.github.io/0.8/config/coverage.html
 		// http://blog.dc.esri.com/2014/03/26/js-code-coverage/
 		reporters: [
-			'progress', 'coverage'
+			'coverage'
 		],
 		preprocessors: {
 		  // source files, that you wanna generate coverage for
