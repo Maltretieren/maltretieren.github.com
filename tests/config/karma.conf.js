cbd97eeb037bfbcfd6818e13c259ae5f18b2944f
crunsh <maltretieren.github.com>
2014-10-21 20:24:36 +0200

    test

--- tests/config/karma.conf.js
+++ tests/config/karma.conf.js
@@ -3,9 +3,7 @@ module.exports = function(config){
         basePath : '../../',
 		// manual definition, this is redundant to /app/js/main.js labjs file, but I didn't manage to reuse...
         files : [
-            // helper
-            '/tests/helper/keypress.js',
-            // same as in /app/js/main.js
+            'tests/helper/keypress.js',
 			'assets/js/jquery-2.1.0/jquery-2.1.0.min.js',
 			'app/js/config.js',
 			'assets/js/oauth/jso.js',
