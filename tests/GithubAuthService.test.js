4b40fd51ea9c135414baef4edb0f927b74df5bf6
crunsh <maltretieren.github.com>
2014-10-21 16:56:13 +0200

    first service test

---
+++ tests/GithubAuthService.test.js
@@ -0,0 +1,30 @@
+'use strict';
+
+describe('GithubAuthService', function() {
+    var $rootScope, $scope, $service;
+
+    // prepare angular for being testable
+    beforeEach(angular.mock.module('myApp'));
+    beforeEach(angular.mock.inject(function(_$rootScope_, _$service_){
+        $rootScope = _$rootScope_;
+        $scope = $rootScope.$new();
+        $service = _$service_;
+        // Mock UserModel
+        UserModel = {
+            getUser: function() {
+                return "{'name':'Maltretieren','isAdmin':true}"
+            }
+        }
+        $service('GithubAuthService', {
+            '$rootScope' : $rootScope,
+            '$scope': $scope,
+            'UserModel': UserModel
+        });
+    }));
+
+    // dependency to UserModel
+    it('startup', function(done) {
+        expect(true).toBeTruthy();
+        done();
+    });
+});
