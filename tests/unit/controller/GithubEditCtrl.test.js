8c87cd9feb606bb3545c12b1cef80856680fd69f
Malteser <MalteAhrens@arcor.de>
2014-10-22 09:23:27 +0200

    test

--- tests/unit/controller/GithubEditCtrl.test.js
+++ tests/unit/controller/GithubEditCtrl.test.js
@@ -4,7 +4,13 @@
 describe('Controller', function() {
     describe('GithubEditCtrl', function () {
         var $scope, $controller, $window, $document, $compile, input;
-
+		function getElement() {
+			var element = angular.element("<textarea id='target-editor'></textarea>");
+			$compile(element)($scope);
+			document.body.appendChild(element[0]);
+			return element;
+		  }
+		
         // prepare angular for being testable
         beforeEach(module('myApp'));
         beforeEach(inject(function (_$rootScope_, _$controller_, _$window_, _$document_, _$compile_) {
@@ -17,11 +23,6 @@ describe('Controller', function() {
                 '$scope': $scope,
                 '$window': $window
             });
-			
-			input = document.createElement("textarea");
-			input.setAttribute('id', 'target-editor');
-			var template = $compile(input)($scope);
-			$scope.$digest();
         }));
 		beforeEach(function() {
 			// http://stackoverflow.com/questions/20313575/angular-js-unit-test-mock-document
@@ -31,18 +32,20 @@ describe('Controller', function() {
 			//var template = $compile(element)(scope);
 			//var body = $document.find('body').eq(0);
 			//body.append(input);
+			 
 		});
 
         it('test the save hotkey', function() {
             // this test is not working for now
-            spyOn($scope, 'save');
+            var element = getElement();
+			spyOn($scope, 'save');
             console.info('Simulating STRG+S hotkey inside the textarea')
 			happen.once(input, {
 				type: 'keypress',
 				keyCode: '83',
 				ctrlKey: true
 			});
-			console("key should be pressed");
+			console.info("key should be pressed");
             //KeyEvent.simulate(19, 19)
             //expect($scope.save).toHaveBeenCalled();
         });
