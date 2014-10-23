f9e166b878ff2dd433bf86fc283bb1ec6b6600b2
crunsh <maltretieren.github.com>
2014-10-23 23:18:41 +0200

    test

--- tests/unit/service/UrlSrvc.test.js
+++ tests/unit/service/UrlSrvc.test.js
@@ -13,12 +13,14 @@ describe('Service', function() {
 
         it('should extract a param out of the browser location', function () {
             var dateTitle = UrlSrvc.parseDateTitle("_posts/2014-10-20-testing-combo.md");
-            console.log(dateTitle);
+            expect(dateTitle.date).toBe('2014-10-20');
+            expect(dateTitle.title).toBe('testing combo');
         });
 
         it('should test parsing of the path variable', function () {
             var url = 'http://abc.com/edit.html?path=_posts/2014-10-20-testing-combo.md&url=/development/2014/10/20/testing-combo'
             var param = UrlSrvc.getParams(url, 'path');
+            expect(param).toBe('_posts/2014-10-20-testing-combo.md');
         });
     });
 });
