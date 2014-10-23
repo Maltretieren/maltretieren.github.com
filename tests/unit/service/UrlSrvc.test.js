0fcf9d54c1f59cf0883df781832baac49096b810
Malteser <MalteAhrens@arcor.de>
2014-10-23 13:15:11 +0200

    tees

--- tests/unit/service/UrlSrvc.test.js
+++ tests/unit/service/UrlSrvc.test.js
@@ -3,17 +3,19 @@
 // group all controller tests together with outer describe
 describe('Service', function() {
     describe('UrlSrvc', function () {
-        var urlSrvc;
+        var urlSrvc, $provide;
 
         // prepare angular for being testable
         beforeEach(module('myApp'));
-        beforeEach(inject(function (_UrlSrvc_) {
+        beforeEach(inject(function (_UrlSrvc_, _$provide_) {
             urlSrvc = _UrlSrvc_;
+			$provide = _$provide_;
         }));
 
         // dependency to UserModel
         it('should test if it can extract params correctly', function () {
-            console.log(window.location.href);
+            $provide.value('$window', {location:{href:'dummy'}});
+			console.log(window.location.href);
         });
     });
 });
