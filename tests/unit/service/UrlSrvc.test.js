8ba515a5bbe3bc59da746d4ecf3948f7a3d33af5
Malteser <MalteAhrens@arcor.de>
2014-10-24 12:48:23 +0200

    test

--- tests/unit/service/UrlSrvc.test.js
+++ tests/unit/service/UrlSrvc.test.js
@@ -4,25 +4,17 @@
 describe('Service', function() {
     describe('UrlSrvc', function () {
         var UrlSrvc;
-		var mockUrlSrvc = {};
 		
         // prepare angular for being testable
 		// http://www.youtube.com/watch?v=qK-Z0oEdE4Y&feature=player_embedded
-		// howto wire up with promises: http://plnkr.co/edit/Fi1SQq?p=preview
         beforeEach(module('myApp'));
-        beforeEach(function () {
-			// mock the function getUrl in UrlSrvc (other functions will stay intact)
-			module(function ($provide) {
-				$provide.value('UrlSrvc', mockUrlSrvc);
-			});
-        });
 		beforeEach(inject(function (_UrlSrvc_) {
-			UrlSrvc = _UrlSrvc_;
-			UrlSrvc.getUrl = function () {
+            UrlSrvc = _UrlSrvc_;
+			UrlSrvc.getUrl = function() {
 				return 'http://abc.com/edit.html?path=_posts/2014-10-20-testing-combo.md&url=/development/2014/10/20/testing-combo';
 			}
-		}));
-
+        }));
+	
         it('should extract a param out of the browser location', function () {
             var dateTitle = UrlSrvc.parseDateTitle("_posts/2014-10-20-testing-combo.md");
             expect(dateTitle.date).toBe('2014-10-20');
@@ -30,9 +22,8 @@ describe('Service', function() {
         });
 
         it('should test parsing of the path variable', function () {
-            // this should use the mocked function
+            // this should use the mocked function 
 			var url = UrlSrvc.getUrl();
-			console.log("url: "+url);
             var param = UrlSrvc.getParams(url, 'path');
             expect(param).toBe('_posts/2014-10-20-testing-combo.md');
         })
