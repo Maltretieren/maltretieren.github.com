4e79adde4012b15bf32eabe9c6e3673a038df72e
Malteser <MalteAhrens@arcor.de>
2014-10-24 11:05:30 +0200

    test

--- tests/unit/service/UrlSrvc.test.js
+++ tests/unit/service/UrlSrvc.test.js
@@ -4,14 +4,17 @@
 describe('Service', function() {
     describe('UrlSrvc', function () {
         var UrlSrvc;
+		var mockGetUrl;
 		
         // prepare angular for being testable
 		// http://www.youtube.com/watch?v=qK-Z0oEdE4Y&feature=player_embedded
         beforeEach(module('myApp'));
         beforeEach(function () {
-			mockGetUrl = function () {
-				return 'http://abc.com/edit.html?path=_posts/2014-10-20-testing-combo.md&url=/development/2014/10/20/testing-combo';
-			}
+			mockGetUrl = {
+				getUrl: function () {
+					return 'http://abc.com/edit.html?path=_posts/2014-10-20-testing-combo.md&url=/development/2014/10/20/testing-combo';
+				}
+			};
 			
 			// mock the function getUrl in UrlSrvc (other functions will stay intact)
 			module(function ($provide) {
