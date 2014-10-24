2e3e369f0e39427804672ea70728d309013e5865
Malteser <MalteAhrens@arcor.de>
2014-10-24 13:59:32 +0200

    mocking in services

--- tests/unit/service/UrlSrvc.test.js
+++ tests/unit/service/UrlSrvc.test.js
@@ -1,29 +1,45 @@
-'use strict';
+'use strict'
 
 // group all controller tests together with outer describe
 describe('Service', function() {
     describe('UrlSrvc', function () {
         var UrlSrvc;
+		var mockGetUrl = {
+			getUrl: function () {
+				return 'http://abc.com/edit.html?path=_posts/2014-10-20-testing-combo.md&url=/development/2014/10/20/testing-combo';
+			},
+			getParams: function(url, paramName) {
+				return '_posts/2014-10-20-testing-combo.md'
+			},
+			parseDateTitle: function(url) {
+				return {
+					date: '2014-10-20',
+					title: 'testing combo'
+				}
+			}
+		}
 		
         // prepare angular for being testable
 		// http://www.youtube.com/watch?v=qK-Z0oEdE4Y&feature=player_embedded
         beforeEach(module('myApp'));
-		beforeEach(inject(function (_UrlSrvc_) {
-            UrlSrvc = _UrlSrvc_;
+        beforeEach(	module(function ($provide) {
+			$provide.value('UrlSrvc', mockGetUrl);
+        }));
+        beforeEach(inject(function (_UrlSrvc_) {
+            UrlSrvc  = _UrlSrvc_;
         }));
 
-	
-        it('should extract a param out of the browser location', function () {
+		 it('should extract a param out of the browser location', function () {
             var dateTitle = UrlSrvc.parseDateTitle("_posts/2014-10-20-testing-combo.md");
             expect(dateTitle.date).toBe('2014-10-20');
             expect(dateTitle.title).toBe('testing combo');
         });
 
+
         it('should test parsing of the path variable', function () {
-            // this should use the mocked function 
-			var url = 'http://abc.com/edit.html?path=_posts/2014-10-20-testing-combo.md&url=/development/2014/10/20/testing-combo';
+            var url = UrlSrvc.getUrl();
             var param = UrlSrvc.getParams(url, 'path');
             expect(param).toBe('_posts/2014-10-20-testing-combo.md');
-        })
-	});
+        });
+    });
 });
