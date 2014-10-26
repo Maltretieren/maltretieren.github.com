18f71821d5c07b2ae7da3b37408d8bca6051b2b5
crunsh <maltretieren.github.com>
2014-10-26 15:20:51 +0100

    styleswitcher test

--- tests/unit/service/StyleSwitcher.test.js
+++ tests/unit/service/StyleSwitcher.test.js
@@ -3,16 +3,29 @@
 // group all controller tests together with outer describe
 describe('Service', function() {
     describe('StylingSwitcher', function () {
-        var StyleSwitcher;
+        var StyleSwitcher, $scope, $compile;
+
+        function getElement() {
+            // Mock the related DOM element
+            var element = angular.element("<link rel='stylesheet'></link>");
+            $compile(element)($scope);
+            document.body.appendChild(element[0]);
+            return element;
+        }
 
         // prepare angular for being testable
         beforeEach(module('myApp'));
-        beforeEach(inject(function (_StyleSwitcher_) {
+        beforeEach(inject(function (_StyleSwitcher_, _$rootScope_, _$compile_) {
             StyleSwitcher = _StyleSwitcher_;
+            $scope = _$rootScope_.$new();
+            $compile = _$compile_;
         }));
 
         it('should test StyleSwitcher', function () {
-            expect(true).toBeTruthy()
+            var element = getElement();
+            var spyStyle = spyOn(StyleSwitcher, 'switch').and.callThrough();
+            StyleSwitcher.switch("test");
+            expect(StyleSwitcher.switch).toHaveBeenCalled();
         });
     });
 });
