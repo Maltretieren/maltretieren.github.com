72a97dcb237b91ee100abec9889b41a6bfeb0649
maltretieren <maltretieren.github.com>
2014-10-28 17:37:36 +0100

    test

--- Gruntfile.js
+++ Gruntfile.js
@@ -51,7 +51,7 @@ module.exports = function(grunt) {
 					template: 'tests/templates/documentation.hbs'
                 },
                 src: "app/js/*.js",
-				dest: "_posts/documentation/"+grunt.template.today('yyyy-mm-dd')+"-documentation.md"
+				dest: "grunt.template.today('yyyy-mm-dd')+"-documentation.md"
             }
         },
         coveralls: {
@@ -105,5 +105,6 @@ module.exports = function(grunt) {
     // Add a new task for travis
     grunt.registerTask('devmode', ['jsdoc2md', 'karma:unit', 'watch'])
     grunt.registerTask('testLocally', ['karma:unit', 'watch'])
-    grunt.registerTask('test', ['karma:travis', 'jsdoc2md', 'protractor:saucelabs'])
+    grunt.registerTask('test', ['karma:travis', 'protractor:saucelabs'])
+    grunt.registerTask('doc', ['jsdoc2md'])
 };
