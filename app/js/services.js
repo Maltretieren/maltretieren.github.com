579328baa49c6beb38aa95a29139c60c65597c46
maltretieren <maltretieren.github.com>
2014-11-01 00:27:34 +0100

    test

--- app/js/services.js
+++ app/js/services.js
@@ -1,11 +1,8 @@
 "use strict";
 
 /**
-  Services are singleton objects used to share data (e.g. among several controllers) and generallyencapsulate reusable pieces of code (since they can be injected and offer their "services" in any part of your app that needs them: controllers, directives, filters, other services etc).
- */
-
-/**
  * @namespace Services
+ * @description   Services are singleton objects used to share data (e.g. among several controllers) and generallyencapsulate reusable pieces of code (since they can be injected and offer their "services" in any part of your app that needs them: controllers, directives, filters, other services etc).
  */
 
 /**
@@ -797,21 +794,22 @@ myApp.service("PollingImgSrvc", function ($q, $timeout) {
  */
 myApp.service("StyleSwitcher", function () {
 	return {
- switch: function(styleName) {
-		console.log("switch to style: "+styleName);
-		if(typeof styleName!=='undefined' && styleName !== '') {
-			var i, link_tag ;
-			  for (i = 0, link_tag = document.getElementsByTagName("link") ;
-				i < link_tag.length ; i++ ) {
-				if ((link_tag[i].rel.indexOf( "stylesheet" ) != -1) &&
-				  link_tag[i].title) {
-				  link_tag[i].disabled = true ;
-				  if (link_tag[i].title == styleName) {
-					link_tag[i].disabled = false ;
-				  }
-				}
-			  }
-		}
-	}}
+        switch: function (styleName) {
+            console.log("switch to style: " + styleName);
+            if (typeof styleName !== 'undefined' && styleName !== '') {
+                var i, link_tag;
+                for (i = 0, link_tag = document.getElementsByTagName("link");
+                     i < link_tag.length; i++) {
+                    if ((link_tag[i].rel.indexOf("stylesheet") != -1) &&
+                        link_tag[i].title) {
+                        link_tag[i].disabled = true;
+                        if (link_tag[i].title == styleName) {
+                            link_tag[i].disabled = false;
+                        }
+                    }
+                }
+            }
+        }
+    }
 });
 
